84a4d89fc11d7e3c668f263fa90f6661065321b9
jerryramos <jerryramos1995@gmail.com>
2020-06-21 13:02:25 -0100

    add card + css

---
+++ src/components/Card/card.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./card.css";
+
+export default class card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <img src={this.props.url} alt="Avatar" width="120px" height="90px" />
+        <div className="container">
+          <h4>
+            <b>{this.props.nome}</b>
+          </h4>
+          <p className="a">{this.props.star}</p>
+          <p className="a">{this.props.star2}</p>
+          <p className="a">{this.props.star3}</p>
+          <p className="a">{this.props.star4}</p>
+          <p className="a">{this.props.star5}</p>
+        </div>
+      </div>
+    );
+  }
+}
