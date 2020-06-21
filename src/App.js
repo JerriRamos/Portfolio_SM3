8347351a178118bcd6f7a410108b34f78584ba23
jerryramos <jerryramos1995@gmail.com>
2020-06-21 10:39:55 -0100

    import comp. MainPag

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 // import "./App.css";
+import MainPag from "./pages/mainPag";
 
 function App() {
-  return <div></div>;
+  return <MainPag />;
 }
 
 export default App;
